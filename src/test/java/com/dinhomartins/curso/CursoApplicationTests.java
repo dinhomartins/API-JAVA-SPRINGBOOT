0c341c3e245b5f9e2c8e966d91b3884e03c6b597
dinhomartins <birawssf@gmail.com>
2023-10-26 16:10:06 -0300

    Base do projeto

---
+++ src/test/java/com/dinhomartins/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinhomartins.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
